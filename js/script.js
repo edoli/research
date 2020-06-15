ac9753a0f97f7c7e6d2f9c57993188e5e6a7ecf1
Daniel S. Jeon <edoli.mia@gmail.com>
2020-06-15 15:43:21 +0900

    Add more informations

--- js/script.js
+++ js/script.js
@@ -13,52 +13,52 @@ function publicationTag(thumbnail, title, authors, conference, materials) {
         ${getThumbnail()}
         
         <div class="flex-fill d-flex flex-column pub-content">
-        <div class="pub-title">${title}</div>
-        <div class="pub-authors">${authors.map((author) => {
-            if (author == 'Daniel S. Jeon') {
-                return `<strong>${author}</strong>`
-            } else {
-                return `${author}`
-            }
-        }).join(', ')}</div>
-        <div class="pub-conference">${conference}</div>
-        <div class="flex-fill"></div>
-        <div>
-        ${materials.map((item) => {
-            let icon = '';
-            let link = '';
-            let path = item.path;
-            let ext = path.split('.').pop().toLowerCase();
-            let isHttp = path.slice(0, 4).toLowerCase() === 'http';
-            let isCode = false;
-            if (isHttp) {
-                icon = '<i class="fas fa-link"></i>';
-                link = `${item.path}`;
-            } else if (isCode) {
-                icon = '<i class="fas fa-file-code"></i>';
-            } else  {
-                if (ext == 'pdf') {
-                    icon = '<i class="fas fa-file-pdf"></i>';
-                } else if (ext == 'zip') {
-                    icon = '<i class="fas fa-file-archive"></i>';
-                } else if (ext == 'mp4' || ext == 'avi' || ext == 'mkv' || ext == 'mov') {
-                    icon = '<i class="fas fa-file-video"></i>';
-                }  else if (ext == 'mp3' || ext == 'ogg' || ext == 'wav' || ext == 'mp4a') {
-                    icon = '<i class="fas fa-file-audio"></i>';
-                } else if (ext == 'ppt' || ext == 'pptx') {
-                    icon = '<i class="fas fa-file-powerpoint"></i>';
-                }  else if (ext == 'xls' || ext == 'xlsx') {
-                    icon = '<i class="fas fa-file-excel"></i>';
-                }  else if (ext == 'doc' || ext == 'docx') {
-                    icon = '<i class="fas fa-file-word"></i>';
+            <div class="pub-title">${title}</div>
+            <div class="pub-authors">${authors.map((author) => {
+                if (author == 'Daniel S. Jeon') {
+                    return `<strong>${author}</strong>`
                 } else {
-                    icon = '<i class="fas fa-file-alt"></i>';
+                    return `${author}`
                 }
-                link = `statics/${item.path}`;
-            }
-            return `<a href="${link}" class="btn btn-primary pub-material">${icon} ${item.name}</a>`;
-        }).join('')}
-        </div>
+            }).join(', ')}</div>
+            <div class="pub-conference">${conference}</div>
+            <div class="flex-fill"></div>
+            <div>
+                ${materials.map((item) => {
+                    let icon = '';
+                    let link = '';
+                    let path = item.path;
+                    let ext = path.split('.').pop().toLowerCase();
+                    let isHttp = path.slice(0, 4).toLowerCase() === 'http';
+                    let isCode = false;
+                    if (isHttp) {
+                        icon = '<i class="fas fa-link"></i>';
+                        link = `${item.path}`;
+                    } else if (isCode) {
+                        icon = '<i class="fas fa-file-code"></i>';
+                    } else  {
+                        if (ext == 'pdf') {
+                            icon = '<i class="fas fa-file-pdf"></i>';
+                        } else if (ext == 'zip') {
+                            icon = '<i class="fas fa-file-archive"></i>';
+                        } else if (ext == 'mp4' || ext == 'avi' || ext == 'mkv' || ext == 'mov') {
+                            icon = '<i class="fas fa-file-video"></i>';
+                        }  else if (ext == 'mp3' || ext == 'ogg' || ext == 'wav' || ext == 'mp4a') {
+                            icon = '<i class="fas fa-file-audio"></i>';
+                        } else if (ext == 'ppt' || ext == 'pptx') {
+                            icon = '<i class="fas fa-file-powerpoint"></i>';
+                        }  else if (ext == 'xls' || ext == 'xlsx') {
+                            icon = '<i class="fas fa-file-excel"></i>';
+                        }  else if (ext == 'doc' || ext == 'docx') {
+                            icon = '<i class="fas fa-file-word"></i>';
+                        } else {
+                            icon = '<i class="fas fa-file-alt"></i>';
+                        }
+                        link = `statics/${item.path}`;
+                    }
+                    return `<a href="${link}" class="btn btn-primary pub-material">${icon} ${item.name}</a>`;
+                }).join('')}
+            </div>
         </div>
     </div>
     `;

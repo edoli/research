c36b6c5ef742148579307521ad773eebea87f975
daniel <edoli.mia@gmail.com>
2022-09-16 15:15:30 +0900

    Add scroll to top

--- js/script.js
+++ js/script.js
@@ -92,3 +92,30 @@ $.getJSON('statics/data.json', (data) => {
         publicationsElem.append(tag);
     });
 });
+
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 128 ||
+    document.documentElement.scrollTop > 128
+  ) {
+    mybutton.style.marginBottom = "0px";
+  } else {
+    mybutton.style.marginBottom = "-72px";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
